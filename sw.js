b4d1ac06e3ae7aa19c9527bc84275beb76160179
brawley1422-alt <brawley1422@gmail.com>
2026-08-28 05:06:19 -0400

    update sw.js 2026-08-28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lineup-202608270404";
+const CACHE = "lineup-202608280404";
 const SHELL = ["/morning-lineup/", "/morning-lineup/index.html"];
 
 self.addEventListener("install", function (e) {
